681c0e957c7ae198ac28425fef283268834e8de6
Andrew Chisholm <awchisholm@gmail.com>
2022-01-31 20:15:18 +0000

    save

--- makebigdb.py
+++ makebigdb.py
@@ -3,7 +3,7 @@ import random
 import pandas as pd
 import sqlite3
 size = range(1,10000000)
-index_values = [str(item).zfill(6) for item in size]
+index_values = [str(item).zfill(8) for item in size]
 values = [random.random() for item in size]
 bigdf = pd.DataFrame({'the_index': index_values, 'not_the_index': index_values, 'value': values})
 con = sqlite3.connect('bigdb.db')
